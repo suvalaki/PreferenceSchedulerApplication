40d9268c79a94e790d818222183a2d3126a6be9c
suvalaki <david.ceglia.diaz@gmail.com>
2018-06-10 16:28:22 +1000

    Initialise Migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,25 @@
-from app import create_app, db, cli
-from app.models import 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-app.create_app()
-cli.register(app)
+class Config(object):
+    # ...
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-@app.shell_context_processor
-def make_shell_context():
-    return {}
+
+
+#app.create_app()
+#cli.register(app)
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+#@app.shell_context_processor
+#def make_shell_context():
+#    return {}
--- main.py
+++ main.py
@@ -1,1 +1,2 @@
-from flask import app
+from app import *
+
